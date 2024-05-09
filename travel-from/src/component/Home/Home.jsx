bf6465c1cdea64cc5a865b19df15e7b0c971e7a1
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-10 00:52:42 +0600

    form setup with react

---
+++ travel-from/src/component/Home/Home.jsx
@@ -0,0 +1,16 @@
+
+import { MdKeyboardArrowDown } from "react-icons/md";
+
+const Home = () => {
+
+    return (
+        <div>
+            <h2 className="font-bold text-3xl max-w-5xl mx-auto my-4">Master Price</h2>
+            <hr />
+            
+
+        </div>
+    );
+};
+
+export default Home;
